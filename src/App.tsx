4b5d43a15a93b47e69014e92fe5a8e9496299441
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2024-01-10 21:54:00 -0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="coral">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="coral">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
